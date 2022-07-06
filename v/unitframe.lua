57aaf7d89dcafb498ef3238e3e6ac27e767eb3dc
hoaxdream <hoaxdream@gmail.com>
2022-07-07 05:38:16 +0800

    unitframe fixes

--- v/unitframe.lua
+++ v/unitframe.lua
@@ -1,16 +1,41 @@
--- Class color unitframe
-local function colorBar(statusbar, unit)
-    local _, class, c
-    if UnitIsPlayer(unit) and UnitIsConnected(unit) and unit == statusbar.unit and UnitClass(unit) then
-        _, class = UnitClass(unit)
-        c = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[class] or RAID_CLASS_COLORS[class]
-        statusbar:SetStatusBarColor(c.r, c.g, c.b)
+-- Class color
+local function colorBar(healthbar, unit)
+    if UnitIsPlayer(unit) and UnitIsConnected(unit) and UnitClass(unit) then
+        _, class = UnitClass(unit);
+        local c = RAID_CLASS_COLORS[class];
+        healthbar:SetStatusBarColor(c.r, c.g, c.b);
+    elseif UnitIsPlayer(unit) and (not UnitIsConnected(unit)) then
+        healthbar:SetStatusBarColor(0.5,0.5,0.5);
+    else
+        healthbar:SetStatusBarColor(0,0.9,0);
     end
 end
+
 hooksecurefunc("UnitFrameHealthBar_Update", colorBar)
 hooksecurefunc("HealthBar_OnValueChanged", function(self)
     colorBar(self, self.unit)
 end)
+
+local function tuiUnitReaction(healthbar, unit)
+    if UnitExists(unit) and (not UnitIsPlayer(unit)) then
+        if (UnitIsTapDenied(unit)) and not UnitPlayerControlled(unit) then
+            healthbar:SetStatusBarColor(0.5, 0.5, 0.5)
+        elseif (not UnitIsTapDenied(unit)) then
+            local reaction = FACTION_BAR_COLORS[UnitReaction(unit,"player")];
+            if reaction then
+                healthbar:SetStatusBarColor(reaction.r, reaction.g, reaction.b);
+            else
+                healthbar:SetStatusBarColor(0,0.6,0.1)
+            end
+        end
+    end
+end
+
+hooksecurefunc("UnitFrameHealthBar_Update", tuiUnitReaction)
+hooksecurefunc("HealthBar_OnValueChanged", function(self)
+    tuiUnitReaction(self, self.unit)
+end)
+
 
 -- Disable unitframe status text
 PlayerHitIndicator:SetText(nil)
